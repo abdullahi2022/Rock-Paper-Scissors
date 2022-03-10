0affd03b9d9cfe3d9d8684c710e4f08677ff04ca
Abdullahi Osman <98919395+abdullahi2022@users.noreply.github.com>
2022-03-09 19:19:10 -0700

    update

--- app.js
+++ app.js
@@ -17,38 +17,38 @@ function generateComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
 
   if (randomNumber === 1) {
-    computerChoice = <h3>'rock'</h3>
+    computerChoice = 'rock'
   }
   if (randomNumber === 2) {
-    computerChoice = <h3>'scissors'</h3>
+    computerChoice = 'scissors'
   }
   if (randomNumber === 3) {
-    computerChoice = <h3>'paper'</h3>
+    computerChoice = 'paper'
   }
   computerChoiceDisplay.innerHTML = computerChoice
 }
 
 function getResult() {
   if (computerChoice === userChoice) {
-    result = <h3>'its a draw!'</h3>
+    result = 'its a draw!'
   }
   if (computerChoice === 'rock' && userChoice === "paper") {
-    result = <h3>'you win!'</h3>
+    result = 'you win!'
   }
   if (computerChoice === 'rock' && userChoice === "scissors") {
-    result = <h3>'you lost!'</h3>
+    result = 'you lost!'
   }
   if (computerChoice === 'paper' && userChoice === "scissors") {
-    result = <h3>'you win!'</h3>
+    result = 'you win!'
   }
   if (computerChoice === 'paper' && userChoice === "rock") {
-    result = <h3>'you lose!'</h3>
+    result = 'you lose!'
   }
   if (computerChoice === 'scissors' && userChoice === "rock") {
-    result = <h3>'you win!'</h3>
+    result = 'you win!'
   }
   if (computerChoice === 'scissors' && userChoice === "paper") {
-    result = <h3>'you lose!'</h3>
+    result = 'you lose!'
   }
   resultDisplay.innerHTML = result
 }
